6924145777e48ee5bd93ff856425f2f58b4d4a3b
Kronx12 <kronx12pr@gmail.com>
2021-11-23 09:42:27 -0800

    add level4

---
+++ level4/source.c
@@ -0,0 +1,20 @@
+void p(char *print) {
+    printf(print);
+    return ;
+}
+
+void n(void) {
+  char local_20c [520];
+  
+  fgets(local_20c,0x200,stdin);
+  p(local_20c);
+  if (m == 0x1025544) {
+    system("/bin/cat /home/user/level5/.pass");
+  }
+  return;
+}
+
+void main() {
+    n();
+    return ;
+}
