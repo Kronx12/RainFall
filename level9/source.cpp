10e3a21c2a814ec63b241ccf73522c4c2fd3e9bc
Kronx12 <kronx12pr@gmail.com>
2021-11-24 16:46:08 -0800

    end level9

---
+++ level9/source.cpp
@@ -0,0 +1,44 @@
+class N {
+  public:
+    N(int param_1) {
+        *(N ***)this = &(N::operator+);
+        _68 = param_1;
+        return;
+    }
+
+    int operator+(N *param_1) {
+      return *(int *)(param_1->_68) + *(int *)(this._68);
+    }
+
+    int operator-(N *param_1) {
+      return *(int *)(this._68) - *(int *)(param_1->_68);
+    }
+
+    void setAnnotation(char *param_1) {
+        size_t __n;
+        
+        __n = strlen(param_1);
+        memcpy(this + 4, param_1, __n);
+        return;
+    }
+
+    N n_on_sait_pas;
+    int _68;
+};
+
+
+
+void main(int argc, char **argv) {
+  N *A;
+  N **B;
+  
+  if (argc < 2) {
+    _exit(1);
+  }
+
+  A = new N(5);
+  B[0] = new N(6);
+  A->setAnnotation(argv[1]);
+  B[0](A);
+  return;
+}
